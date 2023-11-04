e02340675acfa474f4c9f91456cb29362b3cfa04
kinus00 <usuncodes@gmail.com>
2023-11-04 19:14:32 +0000

    done

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,3 +1,9 @@
+node default {
+  file { '/root/README':
+  ensure => file,
+  content => "Welcome to ${fqdn}\n",
+  }
+}
 node puppet.local {
   include role::master
   file {'/etc/secret_password.txt':
